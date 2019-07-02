452162f0d5a6541200fcb4215fdacfe10b164d55
KyleWang <wk52525@126.com>
2019-07-02 17:26:06 +0800

    121. 买卖股票的最佳时机

---
+++ src/dynamic/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,53 @@
+package dynamic;
+
+/**
+ * 121. 买卖股票的最佳时机
+ * @author KyleWang
+ * @version 1.0
+ * @date 2019年07月02日
+ */
+public class BestTimeToBuyAndSellStock {
+
+	/*
+		给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+		如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+		注意你不能在买入股票前卖出股票。
+
+		示例 1:
+		输入: [7,1,5,3,6,4]
+		输出: 5
+		解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+		注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+
+		示例 2:
+		输入: [7,6,4,3,1]
+		输出: 0
+		解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+	*/
+
+	public static void main(String[] args) {
+		int[] prices = { 7, 1, 5, 3, 6, 4 };
+		System.out.println(maxProfit(prices));
+	}
+
+	public static int maxProfit(int[] prices) {
+		// 第i天的最大利润 = max(第i-1天内最大利润，第i天卖出能获得的最大利润(第i天价格-第i天前最低价))
+		if (prices != null && prices.length > 1) {
+			int maxProfit = 0;
+			int floor = prices[0];
+			for (int i = 1; i < prices.length; i++) {
+				int current = prices[i];
+				if (current > floor) {
+					int diff = current - floor;
+					if (diff > maxProfit) {
+						maxProfit = diff;
+					}
+				} else {
+					floor = current;
+				}
+			}
+			return maxProfit;
+		}
+		return 0;
+	}
+}
