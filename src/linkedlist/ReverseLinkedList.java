8c334422db2b619fb1f4b02a3dcef46cb22ba775
KyleWang <wk52525@126.com>
2019-09-06 23:34:27 +0800

    206. 反转链表-遍历解法 24. 两两交换链表中的节点

--- src/linkedlist/ReverseLinkedList.java
+++ src/linkedlist/ReverseLinkedList.java
@@ -3,50 +3,62 @@ package linkedlist;
 import linkedlist.entity.ListNode;
 
 /**
- * 反转链表（206）
+ * 206. 反转链表
  * @author KyleWang
  * @version 1.0
  * @date 2019年05月17日
  */
 public class ReverseLinkedList {
 
-	/*
-	 * 反转一个单链表。
-	 * 
-	 * 示例:
-	 * 
-	 * 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
-	 */
+    /*
+     * 反转一个单链表。
+     *
+     * 示例:
+     *
+     * 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
+     */
 
-	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
-		ListNode second = new ListNode(2);
-		ListNode third = new ListNode(3);
-		ListNode forth = new ListNode(4);
-		ListNode fifth = new ListNode(5);
-		head.next = second;
-		second.next = third;
-		third.next = forth;
-		forth.next = fifth;
-		ListNode.printList(head);
-		ListNode.printList(reverseList(head));
-	}
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, 2, 3, 4, 5);
+        ListNode.printList(head);
+//        ListNode.printList(reverseList(head));
+        ListNode.printList(reverseList2(head));
+    }
 
-	public static ListNode reverseList(ListNode head) {
-		return reverse(null, head);
-	}
+    /**
+     * 解法1：递归
+     */
+    public static ListNode reverseList(ListNode head) {
+        return reverse(null, head);
+    }
 
-	private static ListNode reverse(ListNode preNode, ListNode node) {
-		if (node == null) {
-			return null;
-		}
-		if (node.next == null) {
-			node.next = preNode;
-			return node;
-		} else {
-			ListNode nextNode = node.next;
-			node.next = preNode;
-			return reverse(node, nextNode);
-		}
-	}
+    private static ListNode reverse(ListNode preNode, ListNode node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.next == null) {
+            node.next = preNode;
+            return node;
+        } else {
+            ListNode nextNode = node.next;
+            node.next = preNode;
+            return reverse(node, nextNode);
+        }
+    }
+
+    /**
+     * 解法2：遍历
+     */
+    public static ListNode reverseList2(ListNode head) {
+        ListNode current = head;
+        ListNode pre = null;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = pre;
+            pre = current;
+            current = temp;
+        }
+        return pre;
+    }
+
 }
--- src/linkedlist/entity/ListNode.java
+++ src/linkedlist/entity/ListNode.java
@@ -7,18 +7,37 @@ package linkedlist.entity;
  * @date 2019年05月14日
  */
 public class ListNode {
-	public int val;
-	public ListNode next;
 
-	public ListNode(int x) {
-		val = x;
-	}
+    public int val;
+    public ListNode next;
 
-	public static void printList(ListNode node) {
-		while (node != null) {
-			System.out.print(node.val + " ");
-			node = node.next;
-		}
-		System.out.println();
-	}
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int... arr) {
+        ListNode node = this;
+        node.val = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            node.next = new ListNode();
+            node = node.next;
+            node.val = arr[i];
+        }
+    }
+
+    @Override
+    public String toString() {
+        return val + "";
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
 }
