93ae2a5bdfe930ea80b0801158afdde4a29b2e64
KyleWang <wk52525@126.com>
2020-06-29 14:26:14 +0800

    215. 数组中的第K个最大元素

---
+++ src/array/KthLargestElementInAnArray.java
@@ -0,0 +1,58 @@
+package array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
+/**
+ * 215. 数组中的第K个最大元素
+ * @author KyleWang
+ * @version 1.0
+ * @date 2020年06月29日
+ */
+public class KthLargestElementInAnArray {
+
+    /*
+        在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+        示例 1:
+
+        输入: [3,2,1,5,6,4] 和 k = 2
+        输出: 5
+        示例 2:
+
+        输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+        输出: 4
+        说明:
+
+        你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+     */
+
+    @Test
+    public void test() {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int kthLargest = findKthLargest(arr, 2);
+        Assert.assertEquals(5, kthLargest);
+    }
+
+    @Test
+    public void test2() {
+        int[] arr = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+        int kthLargest = findKthLargest(arr, 4);
+        Assert.assertEquals(4, kthLargest);
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
+        for (int num : nums) {
+            if (queue.size() >= k && queue.peek() < num) {
+                queue.remove();
+                queue.add(num);
+            } else if (queue.size() < k) {
+                queue.add(num);
+            }
+        }
+        return queue.peek();
+    }
+}
