1bb1669bf3e5d641050792a6613479dbe5658f6d
KyleWang <wk52525@126.com>
2020-09-15 09:58:50 +0800

    37. 解数独

--- src/other/SudokuSolver.java
+++ src/other/SudokuSolver.java
@@ -1,5 +1,7 @@
 package other;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 /**
@@ -10,29 +12,29 @@ import java.util.Arrays;
  */
 public class SudokuSolver {
 
-    static boolean[][] rowCheck = new boolean[9][9];
-    static boolean[][] colCheck = new boolean[9][9];
-    static boolean[][] blockCheck = new boolean[9][9];
-    static char[][] board;
 
-    public static void main(String[] args) {
+    @Test
+    public void test() {
         char[][] board = {
-                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-                { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-                { '.', '.', '.', '.', '8', '.', '.', '7', '9' } };
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
         solveSudoku(board);
         for (char[] chars : board) {
             System.out.println(Arrays.toString(chars));
         }
     }
 
-    public static void solveSudoku(char[][] board) {
+    public void solveSudoku(char[][] board) {
+        boolean[][] rowCheck = new boolean[9][9];
+        boolean[][] colCheck = new boolean[9][9];
+        boolean[][] blockCheck = new boolean[9][9];
         // 先读取所有已有数字
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
@@ -45,27 +47,26 @@ public class SudokuSolver {
                 }
             }
         }
-        SudokuSolver.board = board;
-        solve(board.length, 0, 0);
+        solve(board, rowCheck, colCheck, blockCheck, 0, 0);
     }
 
-    private static boolean solve(int n, int row, int col) {
+    private boolean solve(char[][] board, boolean[][] rowCheck, boolean[][] colCheck, boolean[][] blockCheck, int row, int col) {
         // 超出此行范围就向下换一行
-        if (col == n) {
+        if (col == board.length) {
             row++;
             col = 0;
-            if (row == n) {
+            if (row == board.length) {
                 return true;
             }
         }
         if (board[row][col] == '.') {
             for (int i = 1; i <= 9; i++) {
-                if (isValid(i, row, col)) {
+                if (isValid(rowCheck, colCheck, blockCheck, i, row, col)) {
                     board[row][col] = (char) (i + '0');
                     rowCheck[row][i - 1] = true;
                     colCheck[col][i - 1] = true;
                     blockCheck[row / 3 * 3 + col / 3][i - 1] = true;
-                    if (solve(n, row, col + 1)) {
+                    if (solve(board, rowCheck, colCheck, blockCheck, row, col + 1)) {
                         return true;
                     } else {
                         board[row][col] = '.';
@@ -77,10 +78,10 @@ public class SudokuSolver {
             }
             return false;
         }
-        return solve(n, row, col + 1);
+        return solve(board, rowCheck, colCheck, blockCheck, row, col + 1);
     }
 
-    private static boolean isValid(int num, int row, int col) {
+    private boolean isValid(boolean[][] rowCheck, boolean[][] colCheck, boolean[][] blockCheck, int num, int row, int col) {
         return !rowCheck[row][num - 1] && !colCheck[col][num - 1] && !blockCheck[row / 3 * 3 + col / 3][num - 1];
     }
 }
