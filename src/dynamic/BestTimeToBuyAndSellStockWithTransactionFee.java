fdc6521a52f59081e38a791455cd24bf219d5147
KyleWang <wk52525@126.com>
2020-12-17 18:45:27 +0800

    714. 买卖股票的最佳时机含手续费

--- src/dynamic/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/dynamic/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -45,21 +45,17 @@ public class BestTimeToBuyAndSellStockWithTransactionFee {
     }
 
     /**
-     * dp[i] 保存第i天，买入、卖出、持有不操作、未持有不操作 的利润
+     * dp[i] 保存第i天，持有、未持有 的利润
      */
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n][4];
+        int[][] dp = new int[n][2];
         dp[0][0] = -prices[0];
-        dp[0][1] = Integer.MIN_VALUE / 2;
-        dp[0][2] = Integer.MIN_VALUE / 2;
-        dp[0][3] = 0;
+        dp[0][1] = 0;
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][3]) - prices[i];
-            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2]) + prices[i] - fee;
-            dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][2]);
-            dp[i][3] = Math.max(dp[i - 1][1], dp[i - 1][3]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][0] + prices[i] - fee, dp[i - 1][1]);
         }
-        return Math.max(dp[n - 1][1], dp[n - 1][3]);
+        return Math.max(dp[n - 1][0], dp[n - 1][1]);
     }
 }
