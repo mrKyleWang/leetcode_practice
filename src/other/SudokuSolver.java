c1c9d472ee7292b186c3e87ff00f96ea525d950e
KyleWang <wk52525@126.com>
2021-02-08 17:29:06 +0800

    37. 解数独 优化

--- src/other/SudokuSolver.java
+++ src/other/SudokuSolver.java
@@ -32,56 +32,48 @@ public class SudokuSolver {
     }
 
     public void solveSudoku(char[][] board) {
+        int[][] places = new int[81][2];
         boolean[][] rowCheck = new boolean[9][9];
         boolean[][] colCheck = new boolean[9][9];
         boolean[][] blockCheck = new boolean[9][9];
         // 先读取所有已有数字
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+        int pos = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
                 if (c != '.') {
-                    int num = c - '0';
-                    rowCheck[i][num - 1] = true;
-                    colCheck[j][num - 1] = true;
-                    blockCheck[i / 3 * 3 + j / 3][num - 1] = true;
+                    int num = c - '1';
+                    rowCheck[i][num] = colCheck[j][num] = blockCheck[i / 3 * 3 + j / 3][num] = true;
                 }
+                places[pos][0] = i;
+                places[pos++][1] = j;
             }
         }
-        solve(board, rowCheck, colCheck, blockCheck, 0, 0);
+        dfs(board, 0, places, rowCheck, colCheck, blockCheck);
+
     }
 
-    private boolean solve(char[][] board, boolean[][] rowCheck, boolean[][] colCheck, boolean[][] blockCheck, int row, int col) {
-        // 超出此行范围就向下换一行
-        if (col == board.length) {
-            row++;
-            col = 0;
-            if (row == board.length) {
-                return true;
-            }
+    private boolean dfs(char[][] board, int pos, int[][] places, boolean[][] rowCheck, boolean[][] colCheck, boolean[][] blockCheck) {
+        if (pos == 81) {
+            return true;
         }
-        if (board[row][col] == '.') {
-            for (int i = 1; i <= 9; i++) {
-                if (isValid(rowCheck, colCheck, blockCheck, i, row, col)) {
-                    board[row][col] = (char) (i + '0');
-                    rowCheck[row][i - 1] = true;
-                    colCheck[col][i - 1] = true;
-                    blockCheck[row / 3 * 3 + col / 3][i - 1] = true;
-                    if (solve(board, rowCheck, colCheck, blockCheck, row, col + 1)) {
-                        return true;
-                    } else {
-                        board[row][col] = '.';
-                        rowCheck[row][i - 1] = false;
-                        colCheck[col][i - 1] = false;
-                        blockCheck[row / 3 * 3 + col / 3][i - 1] = false;
-                    }
+        int i = places[pos][0];
+        int j = places[pos][1];
+        if (board[i][j] == '.') {
+            for (int num = 0; num < 9; num++) {
+                if (rowCheck[i][num] || colCheck[j][num] || blockCheck[i / 3 * 3 + j / 3][num]) {
+                    continue;
                 }
+                board[i][j] = (char) ('1' + num);
+                rowCheck[i][num] = colCheck[j][num] = blockCheck[i / 3 * 3 + j / 3][num] = true;
+                if (dfs(board, pos + 1, places, rowCheck, colCheck, blockCheck)) {
+                    return true;
+                }
+                rowCheck[i][num] = colCheck[j][num] = blockCheck[i / 3 * 3 + j / 3][num] = false;
+                board[i][j] = '.';
             }
             return false;
         }
-        return solve(board, rowCheck, colCheck, blockCheck, row, col + 1);
-    }
-
-    private boolean isValid(boolean[][] rowCheck, boolean[][] colCheck, boolean[][] blockCheck, int num, int row, int col) {
-        return !rowCheck[row][num - 1] && !colCheck[col][num - 1] && !blockCheck[row / 3 * 3 + col / 3][num - 1];
+        return dfs(board, pos + 1, places, rowCheck, colCheck, blockCheck);
     }
 }
