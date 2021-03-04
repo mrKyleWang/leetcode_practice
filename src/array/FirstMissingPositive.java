0c8a3b27e316c155e3df7a21da58d4f35f2d03e5
KyleWang <wk52525@126.com>
2021-03-04 19:28:00 +0800

    41. 缺失的第一个正数

--- src/array/FirstMissingPositive.java
+++ src/array/FirstMissingPositive.java
@@ -26,10 +26,6 @@ public class FirstMissingPositive {
         输入: [7,8,9,11,12]
         输出: 1
 
-        来源：力扣（LeetCode）
-        链接：https://leetcode-cn.com/problems/first-missing-positive
-        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
         提示：
             你的算法的时间复杂度应为O(n)，并且只能使用常数级别的额外空间。
      */
@@ -63,24 +59,27 @@ public class FirstMissingPositive {
      * 然后再遍历一次数组查当前下标是否和值对应，如果不对应那这个下标就是答案，否则遍历完都没出现那么答案就是数组长度加1。
      */
     public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            int target = nums[i] - 1;
-            while (nums[i] >= 1 && target < nums.length && i != target) {
-                int temp = nums[target];
-                if (temp == nums[i]) {
-                    break;
-                }
-                nums[target] = nums[i];
-                nums[i] = temp;
-                target = temp - 1;
+            int num = nums[i];
+            // 使用while循环判断交换值，因为num所对应的位置可能本身也是一个范围内的整数
+            while (num > 0 && num <= n && i != num - 1 && nums[num - 1] != nums[i]) {
+                swap(nums, num - 1, i);
+                num = nums[i];
             }
         }
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
         return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
 
